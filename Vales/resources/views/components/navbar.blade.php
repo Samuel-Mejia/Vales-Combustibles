49740b6253f21171244fcfd1c9605e66b81e6ca8
Samuel Mejía <79532040+Samuel-Mejia@users.noreply.github.com>
2024-10-30 15:28:32 -0600

    correciones del nav y css de la hora.

--- Vales/resources/views/components/navbar.blade.php
+++ Vales/resources/views/components/navbar.blade.php
@@ -48,7 +48,7 @@
 
         <!-- Botón de menú de hamburguesa (visible solo en pantallas pequeñas) -->
         <button data-collapse-toggle="navbar-sticky" type="button"
-            class="md:hidden inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            class="md:hidden inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-black rounded-lg hover:bg-[#465f79] focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-sticky" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
