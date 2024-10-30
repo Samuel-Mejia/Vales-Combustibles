59081e53757c486ca5a27501921d44cde69547ae
Deadpool345 <87096532+Deadpool345@users.noreply.github.com>
2024-10-30 13:28:49 -0600

    navbar responsive

--- Vales/resources/views/components/scripts.blade.php
+++ Vales/resources/views/components/scripts.blade.php
@@ -2,6 +2,7 @@
 @vite('resources/css/fondo.css')
 
 <script src="{{ asset('js/clock.js') }}"></script>
+<script src="{{ asset('js/clock2.0.js') }}"></script>
     
 
 <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
