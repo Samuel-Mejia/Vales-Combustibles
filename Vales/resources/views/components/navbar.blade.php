d11ee1209054a78c7b436218b1a6efd2044c42d0
Samuel Mejía <79532040+Samuel-Mejia@users.noreply.github.com>
2024-10-31 23:01:24 -0600

    --

--- Vales/resources/views/components/navbar.blade.php
+++ Vales/resources/views/components/navbar.blade.php
@@ -126,7 +126,7 @@
                 <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown2"
                     class="text-white bg-[#30475e] hover:bg-[#465f79] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     type="button">
-                    <span>{{ session('user')->username }}</span>
+                    <span>{{ session('user')->name }}, </span>
                     <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                         viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
